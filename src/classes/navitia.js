c78cb2ce5aa16aabf48bcf76a04b339dc81dc405
Joffrey Janiec <jjaniec@student.42.fr>
2019-03-02 00:31:23 +0100

    subject pdf - begin search for nearest station of coordinates

--- src/classes/navitia.js
+++ src/classes/navitia.js
@@ -5,8 +5,10 @@ const config = require('../../config');
 module.exports.default = class navitia {
 	constructor() {
 		axios.defaults.baseURL = 'https://api.navitia.io';
-		axios.defaults.headers.common['Authorization'] = config.navitia_token;
+		axios.defaults.headers.common['Authorization'] = config.navitia.token;
 		axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+		this.coverage = config.navitia.coverage
+		this.network = config.navitia.network
 
 		this.lines = null;
 		this.stops = {
@@ -15,17 +17,18 @@ module.exports.default = class navitia {
 	}
 
 	getLines() {
-		return axios.get('/v1/coverage/fr-idf/physical_modes/physical_mode%3AMetro/lines//?q=&')
+		return axios.get(`/v1/coverage/${this.coverage}/physical_modes/physical_mode%3AMetro/lines//?q=&`)
 		.then(resp => {
+			this.lines = resp.data
 			return resp.data;
 		})
 		.catch(err => {
 			throw new Error('Failed to fetch lines', err);
 		})
 	}
-	
+
 	async getStopsPage(page) {
-		return axios.get(`/v1/coverage/fr-idf/physical_modes/physical_mode%3AMetro/stop_points//?start_page=${page}&`)
+		return axios.get(`/v1/coverage/${this.coverage}/physical_modes/physical_mode%3AMetro/stop_points//?start_page=${page}&count=383`)
 		.then(resp => {
 			return (resp.data)
 		})
@@ -36,9 +39,50 @@ module.exports.default = class navitia {
 
 	async getStops() {
 		// 383 items on paris with 25 items/page
-		this.stopsPages = await Promise.all(Array(16).fill(0).map((val, index) => this.getStopsPage(index)))
-		this.stops = Object.assign(...this.stopsPages);
-		//console.log(this.stops)
-		return this.stops;
+		try {
+			this.stopsPages = await Promise.all(Array(1).fill(0).map((val, index) => this.getStopsPage(index)))
+			this.stops = Object.assign(...this.stopsPages);
+			//console.log(this.stops)
+			return this.stops;
+		} catch (e) {
+			console.error(e)
+		}
+	}
+
+	async getClosestStop(lon, lat) {
+		return axios.get(`/v1/coverage/${this.coverage}/coord/${lon}%3B${lat}/places_nearby?count=5&type%5B%5D=stop_point&`)
+		.then(resp => {
+			for (var i = 0; i < resp.data.places_nearby.length; i++) {
+				console.log(`i : ${i} - name: |${resp.data.places_nearby[i].name}|`)
+				if (resp.data.places_nearby[i].name === "Porte de Clichy (Paris)") // -> A remplacer par un foreach
+				{
+					tmp = this.stops.stop_points.filter(item => {
+						console.log(item.label)
+						console.log(resp.data.places_nearby[i].name)
+						if (item.label == resp.data.places_nearby[i].name)
+						{
+							console.log("Success")
+						}
+						return item.label == resp.data.places_nearby[i].name
+					})
+					return tmp
+				}
+			}
+			return null;
+		})
+		.catch(err => {
+			throw new Error('Failed to fetch closest metro station', err);
+		})
+	}
+
+	async getRoutes() {
+		return axios.get(`/v1/coverage/${this.coverage}/networks/${this.network}/routes?count=32&depth=3`)
+		.then(resp => {
+			console.log(resp.data)
+			return resp.data
+		})
+		.catch(err => {
+			throw new Error('Failed to fetch routes', err);
+		})
 	}
 }
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const json = require('json')
+const path = require('path')
+const config = require('../config')
+const navitia = require('./classes/navitia').default
+
+const main = async () => {
+	let client = new navitia()
+
+	if (!fs.existsSync('./lines.json'))
+	{
+		let lines = client.getLines()
+		fs.writeFileSync('./lines.json', JSON.stringify(lines, null, 4) , 'utf-8')
+	}
+	if (!fs.existsSync('./stops.json'))
+	{
+		let stops = await client.getStops()
+		fs.writeFileSync('./stops.json', JSON.stringify(stops, null, 4), 'utf-8')
+	}
+	else
+	{
+		await fs.readFile(path.join(__dirname, '/stops.json'), "utf-8", (err, data) => {
+			client.stops = JSON.parse(data)
+		})
+	}
+	if (!fs.existsSync('./routes.json'))
+	{
+		let routes = await client.getRoutes()
+		fs.writeFileSync('./routes.json', JSON.stringify(routes, null, 4), 'utf-8')
+	}
+
+//	2.3193711;48.8963911
+	client.getClosestStop(2.315392, 48.895295)
+}
+
+main()
