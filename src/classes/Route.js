eb57bb302e1f7b999a4adfc02bd325a71191d6c3
cvermand <charlottevermandel@gmail.com>
2019-03-02 18:37:16 +0100

    refractor route.js

--- src/classes/Route.js
+++ src/classes/Route.js
@@ -1,14 +1,10 @@
 class Route{
-	constructor(name, origin, dest) {
+	constructor(origin, dest) {
 		this.origin = origin;
 		this.dest = dest;
 		this.cost = Math.floor(Math.random() * 10);
 		this.line = origin.line.name;
 		this.minutesUntilMetro =  Math.floor(Math.random() * 10);	
-	}
-	getName(){
-		console.log(this.routeName);
-		console.log(onsenfou());
 	}
 }
 
